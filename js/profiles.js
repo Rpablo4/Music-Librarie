90780c1099f102efd1ba11b00abc75c78bb31b7a
Ruan Pablo <222ruanpablo@gmail.com>
2022-05-06 18:59:10 -0300

    Criando arrays de profiles, tracks e playlists

---
+++ js/profiles.js
@@ -0,0 +1,20 @@
+var ArrayProfiles = [
+    {
+        name: "Jenny Villa",
+        albums: "4 albums",
+        lastView: "1 day ago",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\view1.jpg"
+    },
+    {
+        name: "Aquila",
+        albums: "2 albums",
+        lastView: "3 day ago",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\view2.jpg"
+    },
+    {
+        name: "This is War",
+        albums: "5 albums",
+        lastView: "2 week ago",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\view3.jpg"
+    }
+]
---
+++ js/tracks.js
@@ -0,0 +1,32 @@
+var ArrayProfiles = [
+    {
+        title: "Mahara Bon",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\track1.jpg",
+        album: "Sexy in Paris"
+    },
+    {
+        title: "Donna",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\track2.jpg",
+        album: "Pseudo-Life"
+    },
+    {
+        title: "Avven",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\track2.jpg",
+        album: "Magic"
+    },
+    {
+        title: "ThunCony Payton",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\track4.jpg",
+        album: "Living"
+    },
+    {
+        title: "White Rabbit",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\track5.jpg",
+        album: "Saloon"
+    },
+    {
+        title: "Visions",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\track6.jpg",
+        album: "Neon"
+    }
+]
