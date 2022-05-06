0fa34b49ffa752c5857e89ee33d217e42c51dfc6
Ruan Pablo <222ruanpablo@gmail.com>
2022-05-06 18:58:20 -0300

    Criando arrays de playlists, tracks e profiles

---
+++ js/playlist.js
@@ -0,0 +1,27 @@
+var ArrayPlaylist = [
+    {
+        title: "Only STARDUST",
+        tracks: "24 tracks",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\playlist1.jpg"
+    },
+    {
+        title: "Mike Vella",
+        tracks: "12 tracks",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\playlist2.jpg"
+    },
+    {
+        title: "the best singles s2",
+        tracks: "163 tracks",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\playlist3.jpg"
+    },
+    {
+        title: "Only STARDUST",
+        tracks: "24 tracks",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\playlist4.jpg"
+    },
+    {
+        title: "Indie",
+        tracks: "63 tracks",
+        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\playlist4.jpg"
+    }
+]
