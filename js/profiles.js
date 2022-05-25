b2dfef195680e7fb8ffb35200a66aa709097d390
Ruan Pablo <222ruanpablo@gmail.com>
2022-05-24 23:04:23 -0300

    Adicionando POO de profiles

--- js/profiles.js
+++ js/profiles.js
@@ -3,18 +3,18 @@ var ArrayProfiles = [
         name: "Jenny Villa",
         albums: "4 albums",
         lastView: "1 day ago",
-        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\view1.jpg"
+        image: "https://firebasestorage.googleapis.com/v0/b/musiclibrarie-bade8.appspot.com/o/view1.jpg?alt=media&token=57d2d7b4-6b4f-49be-ad86-19c9af29647c"
     },
     {
         name: "Aquila",
         albums: "2 albums",
         lastView: "3 day ago",
-        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\view2.jpg"
+        image: "https://firebasestorage.googleapis.com/v0/b/musiclibrarie-bade8.appspot.com/o/view2.jpg?alt=media&token=0e719c5a-0c3b-4156-8d3d-64af7019e110"
     },
     {
         name: "This is War",
         albums: "5 albums",
         lastView: "2 week ago",
-        image: "C:\Users\222ru\Desktop\CODES\Music-Librarie\assets\view3.jpg"
+        image: "https://firebasestorage.googleapis.com/v0/b/musiclibrarie-bade8.appspot.com/o/view3.jpg?alt=media&token=31dd27ae-4e9b-41c6-adc8-94ba29011e3e"
     }
 ]
---
+++ js/profilesRender.js
@@ -0,0 +1,29 @@
+function constroiTbProfiles(array) {
+    var section = document.querySelector('#renderProfileViews');
+    var elements = document.querySelectorAll('.featuredContainer');+
+
+    array.forEach(profile => {
+        var profileHtml =  `
+          <div class="profileViewsCartao" {
+            constructor() {
+            }
+          } >
+            <div class="profileViewContainer">
+                <div class="coverProfiles">
+                    <img id="coverProfile" src="${profile.image}" alt="${profile.name}" />
+                </div>
+                <div class="profileData">
+                    <div>
+                        <h3>${profile.name}</h3>
+                        <p class="profileLastView" >${profile.lastView}</p>
+                     </div>
+                    <p> ${profile.albums}</p>
+              </div>
+            </div>
+        `;
+        section.innerHTML += profileHtml;
+      })
+    }
+
+    constroiTbProfiles(ArrayProfiles);
+  
